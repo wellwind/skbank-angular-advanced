d2339d3b00f8f33b91cb7b1284515ab6491f44fa
fio <fionn.339@gmail.com>
2018-07-24 15:42:57 +0800

    練習使用別的模組的元件在樣板上

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, DemoComponent],
+  exports: [DemoComponent]
 })
 export class DashboardModule {}
